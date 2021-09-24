034dd0e2a55ac7fbda616079347f7e2aa1be8a01
MarianaNVasquez <mariana.norenav@udea.edu.co>
2021-09-23 19:12:39 -0500

    Se agregan los métodos colors y escribir

--- Codigo/informa2/pimage.cpp
+++ Codigo/informa2/pimage.cpp
@@ -7,14 +7,14 @@ pimage::pimage(string img)
     }
     else{
         cout << "Se cargo exitosamente la imagen" << endl;
-        image = image.convertToFormat(QImage::Format_RGB888);
+        image = image.convertToFormat(QImage::Format_RGB888); // cada pixel tiene 3bytes (3 canales)
     }
 }
 
-QRgb pimage::promcolor(QRgb color1, QRgb color2)
-{
-    return qRgb((qRed(color1)+qRed(color2))/2,(qGreen(color1)+qGreen(color2))/2,(qBlue(color1)+qBlue(color2))/2);
-}
+//QRgb pimage::promcolor(QRgb color1, QRgb color2)
+//{
+//    return qRgb((qRed(color1)+qRed(color2))/2,(qGreen(color1)+qGreen(color2))/2,(qBlue(color1)+qBlue(color2))/2);
+//}
 
 void pimage::resize()
 {
@@ -57,3 +57,53 @@ void pimage::resize()
 }
 
 
+
+void pimage::colors() //Función que permite extraer los colores de la imagen redimensionada
+{
+    for(int f = 0; f < imageE->height(); f++){
+        for(int c = 0; c < imageE->width(); c++){
+            red = imageE->pixelColor(c,f).red();
+            green = imageE->pixelColor(c,f).green();
+            blue = imageE->pixelColor(c,f).blue();
+            if((red == 255) && (green == 255) && (blue == 255)){
+                red = 254;
+                green = 254;
+                blue = 254;
+            }
+            matriz[0][f][c] = red;
+            matriz[1][f][c] = green;
+            matriz[2][f][c] = blue;
+        }
+    }
+}
+
+void pimage::escribir()
+{
+    ofstream archivo;
+    archivo.open("Datos.txt", ios::out);
+
+    archivo << "{" << endl;
+    for(int f = 0; f < 3; f++){
+        archivo << "{" << endl;
+        for(int c = 0; c < pixeles; c++){
+            archivo << "{";
+            for(int d = 0; d < pixeles; d++){
+                archivo << matriz[f][c][d];
+                if(d < pixeles-1) archivo << ",";
+            }
+            if(c < pixeles-1) archivo << "},";
+            else archivo << "}";
+        }
+        archivo << endl;
+        if(f < 2) archivo << "}," << endl;
+        else archivo << "}" << endl;
+    }
+
+    archivo << "};";
+    archivo.close();
+}
+
+pimage::~pimage()
+{
+    delete imageE;
+}
